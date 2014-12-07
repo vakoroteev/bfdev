5a391412465be4e3f4561eb58b77a3754b18bf5a
vakoroteev <vakoroteev@gmail.com>
2014-12-08 01:04:25 +0300

    Update AccountConstants.java

--- my-aping/src/main/java/my/pack/util/AccountConstants.java
+++ my-aping/src/main/java/my/pack/util/AccountConstants.java
@@ -9,14 +9,14 @@ package my.pack.util;
 
 public class AccountConstants {
 
-	public static final String APP_KEY = "KspOqtaum5ZWClvy";
+	public static final String APP_KEY = "456ftygubhkjhl";
 //	public static final String APP_KEY = "NSXWo7DbpUqG9sct";
-	public static final String USERNAME = "mephun";
+	public static final String USERNAME = "hjblknkjnk";
 //	public static final String USERNAME = "struga";
 //	public static final String PASSWORD = "666Vfkmljhjh666";
-	public static final String PASSWORD = "123viks123";
+	public static final String PASSWORD = "hghjhjlkhoui";
 
 	public static final String ENDPOINT_TO_CERTLOGIN = "https://identitysso.betfair.com/api/certlogin";
-	public static final String PATH_TO_PRIVATE_KEY = "/client-2048.p12";
+	public static final String PATH_TO_PRIVATE_KEY = "/cljkl";
 
 }
